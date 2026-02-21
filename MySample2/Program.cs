ff03b73028f1c2c9c23a2160004b49703674387d
ichi6660-sys <ichi@gmail.com>
2026-02-21 11:13:06 +0900

    add practice project (ignore VS temp files)

---
+++ MySample2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace MySample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int age = 23;
+            //string name = "市川";
+
+            //Console.WriteLine("私、年齢{0}ですが、{1}でし", age , name);
+
+            //name = "山田";
+
+            //Console.WriteLine("私、年齢{0}ですが、{1}でし", age, name);
+
+            //int height = 8;
+            //int width = 5;
+            //int area;
+
+            //area = height * width;
+            //Console.WriteLine("面積は{0}です", area);
+
+            //Console.WriteLine("insart your name");
+            //var name = Console.ReadLine();
+            //Console.WriteLine("{0}さん、おはちょ\nうございます", name);
+
+            //double squareRoot = 1.41421356;
+            //double sideLength = 10.0;
+            //double diagonalLength = sideLength * squareRoot;
+            //Console.WriteLine("対角線の長さは{0}です", diagonalLength);
+
+            //var average = 3.28m;
+            //Console.WriteLine(average);
+
+            //var price = 1280m;
+            //var priceIncludingTax = price * 1.08m;
+            //Console.WriteLine(priceIncludingTax);
+
+            //string name = "市川";
+            //Console.WriteLine("私の名前は{0}です", name);
+
+            //var name = "市川";
+            //var age = 19;
+            //Console.WriteLine("{0}さんは、{1}歳です", name, age);
+
+            //var kilometersPerHour = 54;
+            //var hour = 3;
+            //var kilometer = kilometersPerHour * hour;
+            //Console.WriteLine(kilometer);
+
+            //var str = "改行を示すエスケープシーケンスは、\\nです";
+            //Console.WriteLine(str);
+
+            //var price1 = 1100;
+            //var price2 = 800;
+            //var price3 = 1250;
+
+            //var total = price1 + price2 + price3;
+            //Console.WriteLine("合計金額は、{0}ｴﾝです", total);
+
+            //var member = 4;
+            //var perPerson = total / member;
+            //Console.WriteLine("{0}人で割った一人当たりの金額は、{1}ｴﾝです", member , perPerson);
+
+            //var remainder = total % member;
+            //Console.WriteLine("余りは、{0}ｴﾝです", remainder);
+
+            //var total = 998;
+            //var discount = total * 0.1;
+            //var payment = total - discount;
+
+            //Console.WriteLine("割引額は{0}ｴﾝです", discount);
+            //Console.WriteLine("支払額は{0}ｴﾝです2", payment);
+
+            //var x = +100;
+            //var y = -5;
+            //var a = +(x + y);
+            //var b = -(x + y);
+            //Console.WriteLine("a = {0}, b = {1}", a, b);
+
+            //var num = 6;
+            //var dup = num;
+            //Console.WriteLine(num);
+            //Console.WriteLine(dup);
+
+            var num = 10;
+            num += 20;
+            Console.WriteLine(num);
+
+
+
+
+        }
+    }
+}
